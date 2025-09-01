2205223173370af2171952b3981b3d76e8ed7eae
Muhammad Daniyal Tallat <daniyaltallat0@gmail.com>
2025-09-01 01:38:04 -0700

    redux setup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useState } from "react";
 
-// testing comment
+
 export default function Login() {
  
 {/* ++++++++++++++++++++++++++++++++++++++++++ Handle input +++++++++++++++++++++++++++++++++++++++++++++++++++++ */}
